47318f6517241aeae55d5e97cbd51f0c707d115e
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-07-13 23:55:26 +0530

    pages and routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,3 +1,25 @@
-export const App = () => {
-  return <div>App</div>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashbord from "./pages/Dashbord";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashbord" element={<Dashbord />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
+export default App;
