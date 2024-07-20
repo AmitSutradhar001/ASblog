a5fd3a6fe5b65f99b50000a2390f17f253eb8bb9
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-07-21 00:57:07 +0530

    sign up page with the functionality

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import "dotenv/config";
 import userRouter from "./router/user.router.js";
 import authRoute from "./router/auth.router.js";
+import cors from "cors";
 
 const PORT = 8080;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRouter);
